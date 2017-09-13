e2566489a6f241117a85aa6a79c0d2b7e8c9fcb8
mritd <mritd1234@gmail.com>
2017-09-13 14:56:37 +0800

    修正数据目录缺失问题

--- rpmscripts/preinstall.sh
+++ rpmscripts/preinstall.sh
@@ -1,2 +1,7 @@
 getent group consul >/dev/null || groupadd -r consul
 getent passwd consul >/dev/null || useradd -r -g consul -d /var/lib/consul -s /sbin/nologin -c "consul user" consul
+if [ ! -d /var/lib/consul ]; then 
+    mkdir /var/lib/consul
+    chown -R consul:consul /var/lib/consul
+    chmod -R 0755 /var/lib/consul
+fi
